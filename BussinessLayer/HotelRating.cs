bc59709dcc4447d4663a7fbd9ea8004fe82e1183
M1047902 <geetha.kannan@mindtree.com>
2018-08-29 11:58:19 +0530

    formatting error

--- BussinessLayer/HotelRating.cs
+++ BussinessLayer/HotelRating.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using BusinessEntity;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Http.Formatting;
+//using System.Net.Http.Formatting;
 
 namespace BussinessLayer
 {
